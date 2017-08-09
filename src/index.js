6929a15da27bbe3d56cdc4a30d918bb129810d69
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-08 23:17:33 -0600

    added components added state to our input component. added es6 arrow, obj destructoring, controlled state component, added input field and used events to update the state using this.setState() and set the controlled the value using state.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyA6vZJSqLTOjslgtTkl36VYXChwMalsxw8'
+/*
+create component
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+App = class
+
+<App /> instance
+
+
+show comp's html(JSX) on screen
+*/
+
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <App />,
+  document.querySelector('.container')
+)
